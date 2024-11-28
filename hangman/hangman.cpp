90c8265ef376c2896c88a1588e535d44cade3e4b
mugishaj092 <mugishajoseph092@gmail.com>
2024-11-28 08:49:27 +0200

    Will fix hangman

--- hangman/hangman.cpp
+++ hangman/hangman.cpp
@@ -1,56 +1,65 @@
-#include<iostream>
-#include<vector>
-#include<fstream>
-#include<time.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <time.h>
 using namespace std;
-void PrintMessage(string message,bool printTop=true,bool printBottom=true){
-    if(printTop){
-        cout<<"+---------------------------------+"<<endl;
-        cout<<"|";
+
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+    if (printTop)
+    {
+        cout << "+---------------------------------+" << endl;
+        cout << "|";
     }
-    else{
-        cout<<"|";
+    else
+    {
+        cout << "|";
     }
-    bool front=true;
-    for(int i=message.length(); i<33;i++){
-        if(front){
-            message=" "+message;
-        }else{
-            message=message+" ";
+    bool front = true;
+    for (int i = message.length(); i < 33; i++)
+    {
+        if (front)
+        {
+            message = " " + message;
         }
-        front=!front;
+        else
+        {
+            message = message + " ";
+        }
+        front = !front;
     }
-    cout<<message.c_str();
+    cout << message.c_str();
     if (printBottom)
     {
-        cout << "|"<<endl;
+        cout << "|" << endl;
         cout << "+---------------------------------+" << endl;
     }
     else
     {
-        cout << "|"<<endl;
+        cout << "|" << endl;
     }
 }
 
-void DrawHangman(int guesCount=0){
-    if(guesCount>=1) 
-        PrintMessage("|",false,false);
-    else 
-        PrintMessage("",false,false);
-    if(guesCount>=2) 
-        PrintMessage("|",false,false);
-    else 
-        PrintMessage("",false,false);
-    if(guesCount>=3) 
-        PrintMessage("0",false,false);
-    else 
-        PrintMessage("",false,false);
-    if(guesCount==4) 
-        PrintMessage("/  ",false,false);
-    if(guesCount==5) 
-        PrintMessage("/| ",false,false);
-    if(guesCount>=6) 
-        PrintMessage("/|\\",false,false);
+void DrawHangman(int guesCount = 0)
+{
+    if (guesCount >= 1)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+    if (guesCount >= 2)
+        PrintMessage("|", false, false);
+    else
+        PrintMessage("", false, false);
+    if (guesCount >= 3)
+        PrintMessage("0", false, false);
+    else
+        PrintMessage("", false, false);
+    if (guesCount == 4)
+        PrintMessage("/  ", false, false);
+    if (guesCount == 5)
+        PrintMessage("/| ", false, false);
+    if (guesCount >= 6)
+        PrintMessage("/|\\", false, false);
     else
         PrintMessage("", false, false);
     if (guesCount >= 7)
@@ -63,99 +72,124 @@ void DrawHangman(int guesCount=0){
         PrintMessage("/ \\", false, false);
 }
 
-void PrintLetters(string input ,char from,char to){
+void PrintLetters(string input, char from, char to)
+{
     string s;
-    for(char i=from;i<=to;i++){
-        if(input.find(i)==string::npos){
-            s+=i;
-            s+=" ";
+    for (char i = from; i <= to; i++)
+    {
+        if (input.find(i) == string::npos)
+        {
+            s += i;
+            s += " ";
         }
         else
-            s+="  ";
+            s += "  ";
     }
-    PrintMessage(s,false,false);
+    PrintMessage(s, false, false);
 }
 
-void PrintAvailableLetters(string taken){
+void PrintAvailableLetters(string taken)
+{
     PrintMessage("AVAILABLE LETTER");
     PrintLetters(taken, 'A', 'M');
     PrintLetters(taken, 'N', 'Z');
 }
-bool PrintWordAndCheckWinner(string word,string guessed){
-    bool won =true;
-    string s ;
-    for(int i=0;i<word.length();i++){
-        if(guessed.find(word[i])==string::npos){
+
+bool PrintWordAndCheckWinner(string word, string guessed)
+{
+    bool won = true;
+    string s;
+    for (int i = 0; i < word.length(); i++)
+    {
+        if (guessed.find(word[i]) == string::npos)
+        {
             won = false;
-            s+="_ ";
+            s += "_ ";
         }
-        else 
-            s+=word[i];
-            s+=" ";
+        else
+            s += word[i];
+        s += " ";
     }
-    PrintMessage(s,false,true);
+    PrintMessage(s, false, true);
     return won;
 }
-string LoadRondomWords(){
+
+string LoadRondomWords()
+{
     int lineCount = 0;
     string word;
     vector<string> v;
     ifstream reader("words.txt");
-    if(reader.is_open()){
-        while (getline(reader,word))
+    if (reader.is_open())
+    {
+        while (getline(reader, word))
         {
             v.push_back(word);
-            int randomLine=rand() % v.size();
-            word=v.at(randomLine);
         }
-        
+        int randomLine = rand() % v.size();
+        word = v.at(randomLine);
     }
     return word;
 }
 
-int TriesLeft(string word,string guessed){
+int TriesLeft(string word, string guessed)
+{
     int error = 0;
     for (int i = 0; i <= guessed.length(); i++)
     {
-       if(word.find(guessed[i])==string::npos){
-        error++;
-       }
+        if (word.find(guessed[i]) == string::npos)
+        {
+            error++;
+        }
     }
     return error;
 }
+
 int main()
 {
     srand(time(0));
-    string wordToGuess;
-    wordToGuess = LoadRondomWords();
-    string gueses;
-    int tries=0;
-    bool win=false;
+    char playAgain;
+
     do
     {
-        system("cls");
-        PrintMessage("HANG MAN IS COOL");
-        DrawHangman(10-tries);
-        PrintAvailableLetters(gueses);
-        PrintMessage("Guessed Word");
-        win=PrintWordAndCheckWinner(wordToGuess, gueses);
-        if(win) break;
+        string wordToGuess = LoadRondomWords();
+        string gueses;
+        int tries = 0;
+        bool win = false;
 
-        char x;
-        cout<<"> ";
-        cin>>x;
+        do
+        {
+            system("cls");
+            PrintMessage("HANG MAN IS COOL");
+            DrawHangman(10 - tries);
+            PrintAvailableLetters(gueses);
+            PrintMessage("Guessed Word");
+            win = PrintWordAndCheckWinner(wordToGuess, gueses);
+            if (win)
+                break;
 
-        if(gueses.find(x)==string::npos){
-            gueses+=x;
+            char x;
+            cout << "> ";
+            cin >> x;
+
+            if (gueses.find(x) == string::npos)
+            {
+                gueses += x;
+            }
+            tries = TriesLeft(wordToGuess, gueses);
+        } while (tries < 10);
+
+        if (win)
+        {
+            PrintMessage("YOU WON!");
         }
-        tries=TriesLeft(wordToGuess,gueses);
-    } while (tries<10);
-    if(win){
-        PrintMessage("YOU WON!");
-    }
-    else
-        PrintMessage("Game Over");
-    
+        else
+            PrintMessage("Game Over");
+
+        cout << "Do you want to play again? (Y/N): ";
+        cin >> playAgain;
+    } while (playAgain == 'Y' || playAgain == 'y');
+
     system("pause");
     return 0;
 }
